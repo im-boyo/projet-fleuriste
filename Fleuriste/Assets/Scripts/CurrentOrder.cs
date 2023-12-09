e7df493456823c89108c17fd74c65b15392facfa
im-boyo <felixtradovanovic@gmail.com>
2023-12-09 18:07:18 -0500

    gsfg

--- Fleuriste/Assets/Scripts/CurrentOrder.cs
+++ Fleuriste/Assets/Scripts/CurrentOrder.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CurrentOrder : MonoBehaviour
 {
     public int flowerValue;
+    public GameObject flowerPrefab;
+    private Vector3 flowerPosition;
+
     // Start is called before the first frame update
 
     private void OnTriggerEnter(Collider other)
@@ -15,20 +18,25 @@ public class CurrentOrder : MonoBehaviour
             Debug.Log(gameflow.bouquetValue + "  " + gameflow.orderValue);
         }
     }
-
+    void Start()
+    {
+        flowerPosition = transform.position;
+    }
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Bouquet")
         {
             gameflow.bouquetValue -= flowerValue;
             Debug.Log(gameflow.bouquetValue + "  " + gameflow.orderValue);
+        } else if (other.tag == "Pot")
+        {
+            GameObject flowerPrefabCopy = Instantiate(flowerPrefab, flowerPosition, Quaternion.identity);
+            flowerPrefabCopy.GetComponent<Rigidbody>().isKinematic = false;
+            Debug.Log("It's out");
         }
     }
 
-    void Start()
-    {
-
-    }
+   
 
     // Update is called once per frame
     void Update()
